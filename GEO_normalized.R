249a2fdcacd6969fb71240e62fd2683ebe3f562b
LeiGao922 <58405674+LeiGao922@users.noreply.github.com>
2019-12-01 20:48:28 -0500

    Update GEO_normalized.R

--- GEO_normalized.R
+++ GEO_normalized.R
@@ -26,3 +26,25 @@ if (LogC) { ex[which(ex <= 0)] <- NaN
 exprSet <- log2(ex)
 print("log2 transform finished")}else{print("log2 transform not needed")}
 
+##获取平台对应注释R包（https://www.jianshu.com/p/f6906ba703a0）
+library("illuminaHumanv3.db")
+#获取探针
+probe2symbol_df <- toTable(get("illuminaHumanv3SYMBOL"))
+#使用相同的名称
+names(probe2symbol_df)[1]
+exprSet=data.frame(exprSet,check.names = F)
+exprSet$probe_id=row.names(exprSet)
+library(dplyr)
+exprSet=dplyr::inner_join(exprSet,probe2symbol_df,by="probe_id")
+#看一下symbol有没有重复，发现只有41932个，所以需要去重
+###探针转换以及去重
+str(exprSet)
+exprSet2 <- exprSet %>% 
+  select(-probe_id) %>%  #去掉多余信息
+  select(symbol,1:(ncol(exprSet)-1)) %>%  #重新排列，
+  mutate(rowMean = rowMeans(exprSet[,1:(ncol(exprSet)-2)])) %>% 
+  arrange(desc(rowMean)) %>% #把表达量的平均值按从大到小排序
+  distinct(symbol,.keep_all = T) %>% # symbol留下第一个
+  select(-rowMean) #反向选择去除rowMean
+
+save(exprSet2,file = "exprSet2_symbol.Rda")
